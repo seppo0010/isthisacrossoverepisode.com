903b59d005814d6295d2f5011da3757a127d081e
Sebastian Waisbrot <seppo0010@gmail.com>
2022-05-30 15:30:54 -0300

    Episode detail

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ interface SearchResult {
   stillPath: string
   html: string
   text: string
+  season: number
+  episode: number
+  id: string
 }
 
 function App () {
@@ -15,6 +18,7 @@ function App () {
   const [workerInstance, setWorkerInstance] = useState<any | null>(null)
   const [searchCriteria, setSearchCriteria] = useState('')
   const [searchResults, setSearchResults] = useState<SearchResult[]>([])
+  const [selectedItem, setSelectedItem] = useState<SearchResult | null>(null)
 
   useEffect(() => {
     if (workerInstance) return
@@ -47,21 +51,32 @@ function App () {
   return (
     <div>
       <h1>Is this a cross-over episode?</h1>
-      <label>
+      {selectedItem === null && <label>
         <span id="search">Search</span>
         <input type="text" placeholder="Peanutbutter" value={searchCriteria} onChange={(event) => setSearchCriteria(event.target.value)} aria-labelby="search" />
-      </label>
-      {ready && searchResults && <div>
+      </label>}
+      {/* these should be components, but I don't want to be coding front-end */}
+      {ready && searchResults.length > 0 && selectedItem === null && <div>
         <ul aria-description="Search results">
           {searchResults.map((doc: SearchResult) => (<li key={doc.stillPath}>
-            <button title={doc.text}>
+            <button title={doc.text} onClick={() => setSelectedItem(doc)}>
               <img src={`data/${doc.stillPath}`} alt={doc.text} />
             </button>
           </li>))}
         </ul>
       </div>}
-      {(!ready || !searchResults) && <div>
+      {(!ready || searchResults.length === 0) && selectedItem === null && <div>
         Search your favorite BoJack Horseman&apos;s scenes!
+      </div>}
+      {selectedItem !== null && <div id="selectedItem">
+        <button onClick={() => setSelectedItem(null)}>Back</button>
+        <img src={`data/${selectedItem.stillPath}`} alt={'' /* no alt text as the caption will be below */} />
+        <p>
+          Season {selectedItem.season} / {' '}
+          Episode {selectedItem.episode}{' '}
+          ({new Date(parseInt(selectedItem.id, 10)).toISOString().substr(11, 8)})
+        </p>
+        <p dangerouslySetInnerHTML={{ __html: selectedItem.html }} />
       </div>}
     </div>
   )
