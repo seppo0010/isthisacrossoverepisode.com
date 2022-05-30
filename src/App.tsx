a07de819d8016582636288299ef15786cf29cf3a
Sebastian Waisbrot <seppo0010@gmail.com>
2022-05-30 09:03:47 -0300

    Custom search

--- src/App.tsx
+++ src/App.tsx
@@ -13,14 +13,23 @@ function App () {
     fetch('data/index.json')
       .then((res) => res.text())
       // minisearch configuration must match datamaker's¬
-      .then((data) => setIndex(MiniSearch.loadJSON(data, { fields: ['text'], storeFields: ['html', 'season', 'episode', 'stillPath'] })))
+      .then((data) => setIndex(MiniSearch.loadJSON(data, {
+        fields: ['text'],
+        storeFields: ['html', 'season', 'episode', 'stillPath'],
+        searchOptions: {
+          prefix: true,
+          fuzzy: 0.2
+        }
+      })))
       // TODO: handle error
   })
+  const [searchCriteria, setSearchCriteria] = useState('')
   return (
     <div>
-      {index && <div>
+      <input type="text" placeholder="Peanutbutter" value={searchCriteria} onChange={(event) => setSearchCriteria(event.target.value)} />
+      {index && searchCriteria && <div>
         <ul>
-          {index.search('horse').map((doc: SearchResult) => (<li key={doc.stillPath}>
+          {index.search(searchCriteria).map((doc: SearchResult) => (<li key={doc.stillPath}>
             <img src={`data/${doc.stillPath}`} />
             {doc.html}
           </li>))}
